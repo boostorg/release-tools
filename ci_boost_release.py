0ed6a073c717f0c774b6ed8f1ce839790349743f
Rene Rivera <grafikrobot@gmail.com>
2017-06-13 20:37:15 -0500

    To options needed for auto-index stuff.

--- ci_boost_release.py
+++ ci_boost_release.py
@@ -161,6 +161,7 @@ class script(script_common):
             '--distdir=%s'%(os.path.join(self.build_dir,'dist')),
             '--release-build',
             'auto-index=on' if enable_auto_index else 'auto-index=off',
+            '--enable-index' if enable_auto_index else '',
             parallel=True)
         while doc_build.is_alive():
             time.sleep(3*60)
