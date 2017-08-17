ccddc3c501a3f68d3589aec3f29f1ab32d5d2323
Marshall Clow <marshall@idio.com>
2017-08-16 17:05:29 -0700

    Remove trailing comma from the JSON - thanks to Nana Sankiska for the report

--- ci_boost_release.py
+++ ci_boost_release.py
@@ -236,7 +236,7 @@ class script(script_common):
                 '"sha256":"%s",'%(sha256_sum),
                 '"file":"%s",'%(archive_file),
                 '"branch":"%s",'%(self.branch),
-                '"commit":"%s",'%(self.commit),
+                '"commit":"%s"'%(self.commit),
                 "}")
         
         # List the results for debugging.
