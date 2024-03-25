38ed0545ff2e0cd1f27a3aa6fbd0576d474acfec
sdarwin <samuel.d.darwin@gmail.com>
2024-03-25 06:43:43 -0600

    build_docs: add ruby-dev package

--- build_docs/linuxdocs.sh
+++ build_docs/linuxdocs.sh
@@ -171,7 +171,7 @@ if [ "$skippackagesoption" != "yes" ]; then
         sudo apt-get install -y bison docbook docbook-xml docbook-xsl flex libfl-dev libsaxonhe-java xsltproc
     fi
     if [ "$typeoption" = "main" ]; then
-        sudo apt-get install -y python3-pip ruby
+        sudo apt-get install -y python3-pip ruby ruby-dev
         sudo apt-get install -y bison docbook docbook-xml docbook-xsl docutils-doc docutils-common flex ghostscript graphviz libfl-dev libsaxonhe-java python3-docutils texlive texlive-latex-extra xsltproc
         # the next two gems are for asciidoctor-pdf
         sudo gem install public_suffix --version 4.0.7		# 4.0.7 from 2022 still supports ruby 2.5. Continue to use until ~2024.
